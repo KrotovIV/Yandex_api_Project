309f4d45694464f49bd50ffea285741d0f3f4653
KrotovIV <Krotowivan@yandex.ru>
2022-02-01 16:57:17 +0500

    1

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+import io
+import requests
 
 WIDTH = 360
 HEIGHT = 480
@@ -14,12 +16,41 @@ def input_params():
     scale = input()
     return coords, scale
 
+def make_params(coords, scale):
+    params = {
+        "ll": coords,
+        "spn": scale,
+        "l": "map"
+    }
+    return params
 
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+def get_image(params):
+    api = "http://static-maps.yandex.ru/1.x/"
+    response = requests.get(api, params=params)
+    return io.BytesIO(response.content)
 
-    screen.fill(pygame.Color("black"))
-    pygame.display.flip()
+if __name__ == "__main__":
+    pygame.init()
+
+    WIDTH = 360
+    HEIGHT = 480
+    FPS = 30
+
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    clock = pygame.time.Clock()
+
+    coords, scale = input_params()
+    params = make_params(coords, scale)
+    content = get_image(params)
+
+    image = pygame.image.load(content)
+
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.blit(image, image.get_rect())
+        pygame.display.flip()
